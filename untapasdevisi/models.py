b9e423cdc19fa4cc867a048d4ce3c12dd78ac966
dbarbera <diego@dbarbera.name>
2014-02-24 20:42:54 +0100

    añade comentarios en los metodos a reescribir

--- untapasdevisi/models.py
+++ untapasdevisi/models.py
@@ -1,21 +1,30 @@
 class User():
+    # REESCRIBIR
+    # cambiar los atributos de la clase como os sea necesario para vuestra base de datos
     def __init__(self, username):
         self.username = username
 
-    # reescribir estos metodos para que funcionen con la base de datos que useis
+    # REESCRIBIR
+    # este metodo recibe un nombre de usuario y contraseña y debe devolver el usuario asociado a ellos
+    # si la contraseña es incorrecta o hay algun error devolver null
     @staticmethod
     def authenticate(username, password):
         return User(username)
 
+    # REESCRIBIR
+    # este metodo debe crear un nuevo usuario en la base de datos y devolverlo
+    # si hay algun error devolver null
     @staticmethod
     def register(username, password):
         return User(username)
 
+    # REESCRIBIR
+    # este metodo recive como argumento el atributo que useis en el metodo get_id que esta
+    # mas abajo y debe devolver el usuario asociado a esa id
     @staticmethod
-    def get(username):
-        return User(username)
+    def get(id):
+        return User(id)
 
-    # metodos usados por Flask-login, reescribirlos que funcionen con la base de datos que useis
     def is_authenticated(self):
         return True
 
@@ -25,5 +34,8 @@ class User():
     def is_anonymous(self):
         return False
 
+    # REESCRIBIR
+    # este metodo debe retornar el atributo que va a userse para la cookie. Lo normal seria devolver
+    # la key del modelo, el id, o lo que sea equivalente en vuestras bases de datos
     def get_id(self):
         return self.username
