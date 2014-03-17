8a07a6e7622113970b091c96ab14b14f00d6bccd
dbarbera <diego@dbarbera.name>
2014-03-18 00:18:52 +0100

    added validation email template

--- untapasdevisi/utils.py
+++ untapasdevisi/utils.py
@@ -1,19 +1,22 @@
 import uuid
 import requests
+from flask import render_template
 
 
 def generate_key():
     return uuid.uuid4().hex
 
 
-def send_validation_email(email, key):
+def send_validation_email(user, key):
     url = "https://api.mailgun.net/v2/sandbox74944.mailgun.org/messages"
     # TODO: No poner la key de mailgun hardcoded
     auth = ("api", "key-84mxu54004c5y47zgym0z-d34jnsvl18")
+    # TODO: No poner localhost hardcoded
+    host = "127.0.0.1:5000"
     data = {
         "from": "UnTapasDevisi <untapasdevisi@sandbox74944.mailgun.org>",
-        "to": [email],
+        "to": [user.email],
         "subject": "Bienvenido a UntapasDevisi",
-        "html": '<p>Pulsa sobre el enlace para activar tu cuenta:<br><a href="http://127.0.0.1:5000/keys/' + key + '">http://127.0.0.1:5000/keys/' + key + '</a>'
+        "html": render_template("validation_email.html", host=host, user=user, key=key)
     }
     return requests.post(url, auth=auth, data=data)
