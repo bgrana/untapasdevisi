cd753c8b49da9dd99c046a0d71c20d0f0ce37d53
dbarbera <diego@dbarbera.name>
2014-05-12 02:13:54 +0200

    improve seed

--- seed.py
+++ seed.py
@@ -1,26 +1,37 @@
+# -*- coding: utf-8 -*-
+
 import untapasdevisi.models as models
 from untapasdevisi.forms import RegisterForm
 
 models.connect_db('untapasdevisi')
 
-users = [
-    models.User.register(RegisterForm(username="dbarbera", password="qwerty",
-        email="diego@dbarbera.name", firstname="Diego", lastname="Barberá")),
-    models.User.register(RegisterForm(username="jesus", password="qwerty",
-        email="jesus@vatican.va", firstname="Jesus"))
-]
+# TODO: refactor register to not use form
 
-locals = [
-    models.Local.create_local(
-        name="Bar Paco",
-        location="Madrid"
-    ),
-    models.Local.create_local(
-        name="Bar Pepe",
-        location="Madrid"
-    ),
-    models.Local.create_local(
-        name="Restaurante Chino Gran Sol",
-        location="Madrid"
-    )
-]
+dbarbera = models.User.register(RegisterForm(
+    username="dbarbera",
+    password="qwerty",
+    email="diego@dbarbera.name",
+    firstname="Diego",
+    lastname="Barberá"
+))
+
+dbarbera.activated = True
+dbarbera.save()
+
+jesus = models.User.register(RegisterForm(
+    username="jesus",
+    password="qwerty",
+    email="jesus@vatican.va",
+    firstname="Jesús"
+))
+
+bpaco = models.Local.create_local(name="Bar Paco", location="Madrid")
+bpepe = models.Local.create_local(name="Bar Pepe", location="Madrid")
+chino = models.Local.create_local(name="Restaurante Chino Gran Sol", location="Madrid")
+
+models.Like.create(bpaco, dbarbera)
+models.Like.create(bpaco, jesus)
+models.Like.create(chino, dbarbera)
+
+friendship = models.Friendship.create(dbarbera, jesus)
+friendship.confirm()
